93a2f90d74098661efdb29114287f02711ea7efd
lemontreexf <lemontreexf@163.com>
2017-03-03 15:38:41 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/summerrain/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.example.summerrain.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by summerrain on 2017/3/3.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String CountyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyName() {
+        return CountyName;
+    }
+
+    public void setCountyName(String countyName) {
+        CountyName = countyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
